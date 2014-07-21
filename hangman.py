a08c3bff989b6fcbe54d902f577b76fdf7893e31
Aditi Bhandari <aditibhandari24@gmail.com>
2014-07-20 20:36:00 -0400

    Cleaned up code and renamed function and variables

--- hangman.py
+++ hangman.py
@@ -1,18 +1,20 @@
 import random
+
+
 myList = ["elephant", "carrot", "laptop", "zebra", "kangaroo", "mouse", "notebook"]
 random.choice(myList)
 
 def tries(10):
 	while tries == 10:
-..
+# TODO
 		tries = tries - 1
 
-def lets_play(letter):
-	raw_input('Type a letter: {}') .format(letter)
+def play():
+	letter = raw_input('Type a letter: ') 
 	if letter in myList:
-		return letter
+		print letter
 		raw_input("That's right")
-		return lets_play
+		play()
 	else:
-		return 'Sorry that was wrong, try again!'
-		return lets_play
+		print 'Sorry that was wrong, try again!'
+		play()
