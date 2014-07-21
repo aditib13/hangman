becd9f7f199a880545403a23b11eb8b4536088d7
Aditi Bhandari <aditibhandari24@gmail.com>
2014-07-20 20:43:10 -0400

    testing branches multiple spaces

--- hangman.py
+++ hangman.py
@@ -2,7 +2,10 @@ import random
 
 
 myList = ["elephant", "carrot", "laptop", "zebra", "kangaroo", "mouse", "notebook"]
+
 random.choice(myList)
+
+
 
 def tries(10):
 	while tries == 10:
@@ -11,10 +14,19 @@ def tries(10):
 
 def play():
 	letter = raw_input('Type a letter: ') 
+
+
+
 	if letter in myList:
+	
 		print letter
 		raw_input("That's right")
+	
 		play()
 	else:
+	
 		print 'Sorry that was wrong, try again!'
+	
+
 		play()
+
